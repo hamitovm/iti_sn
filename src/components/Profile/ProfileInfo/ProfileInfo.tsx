de71299870d5c62884d9daa9dd9a24ac7e476dd8
hamitovm <hamitovm@gmail.com>
2022-10-20 01:13:02 +0300

    71 - React JS - setState, local state

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import mod from './ProfileInfo.module.css'
 import {ProfileType} from "../../../redux/profile-reducer";
 import {Preloader} from "../../common/preloader";
+import {ProfileStatus} from "../ProfileStatus/ProfileStatus";
 
 export type ProfileInfoPropsType = {
     userProfile: null | ProfileType
@@ -10,13 +11,13 @@ export type ProfileInfoPropsType = {
 
 export const ProfileInfo = (props: ProfileInfoPropsType) => {
     if (!props.userProfile) {
-        return <Preloader />
+        return <Preloader/>
     }
     return (<div>
             <div className={mod.content_background}>
             </div>
             <img src={props.userProfile.photos.large ? props.userProfile.photos.large : ''} alt=""/>
-
+            <ProfileStatus status={'Hello'}/>
         </div>
 
     )
